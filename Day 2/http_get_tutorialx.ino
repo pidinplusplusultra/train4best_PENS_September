4c96d30755fcec8d3bb4813c36b6094e6889d8b6
pidinplusplusultra <143205946+pidinplusplusultra@users.noreply.github.com>
2023-08-25 22:52:53 +0700

    Add files via upload

---
+++ Day 2/http_get_tutorialx.ino
@@ -0,0 +1,47 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+  
+const char* ssid = "edma21";
+const char* password =  "08125398113";
+  
+void setup() {
+  
+  Serial.begin(115200);
+  delay(4000);
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+  
+  Serial.println("Connected to the WiFi network");
+  
+}
+  
+void loop() {
+  
+  if ((WiFi.status() == WL_CONNECTED)) { //Check the current connection status
+  
+    HTTPClient http;
+  
+    http.begin("https://www.google.com/"); //Specify the URL
+    int httpCode = http.GET();                                        //Make the request
+  
+    if (httpCode > 0) { //Check for the returning code
+  
+        String payload = http.getString();
+        Serial.println(httpCode);
+        Serial.println(payload);
+      }
+  
+    else {
+      Serial.println("Error on HTTP request");
+    }
+  
+    http.end(); //Free the resources
+  }
+  
+  delay(10000);
+  
+}
